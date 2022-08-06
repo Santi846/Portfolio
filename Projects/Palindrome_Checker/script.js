823d7ce77adb365c16588144c8c109582b9b96d0
Santiago Arocha <arochas846@gmail.com>
2022-08-06 20:15:32 -0300

    Up to date

---
+++ Projects/Palindrome_Checker/script.js
@@ -0,0 +1,22 @@
+function palindrome(str) {
+
+    const lowerCase = str.toLowerCase;
+    
+    const reverse = lowerCase.reverse;
+    
+    //const split = replace.split;
+    
+    const replace = reverse.replace;
+    
+    //const join = reverse.join;
+    
+    if (replace){
+          return true;
+    }
+    else {
+          return false;
+    }
+    
+    }
+    
+    palindrome("eye");
